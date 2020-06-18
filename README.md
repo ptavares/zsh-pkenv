b3eceee7b7c1cd0b77b987dcf5c36e9f7ae08a97
Patrick Tavares <tavarespatrick01@gmail.com>
2020-06-18 22:16:33 +0200

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# zsh-pkenv
+
+zsh plugin for installing and loading [pkenv](https://github.com/iamhsa/pkenv.git)
+>Inpired by [zsh-pyenv](https://github.com/mattberther/zsh-pyenv)
+
+## Table of content
+
+- [zsh-pkenv](#zsh-pkenv)
+  - [Usage](#usage)
+  - [Updating pkenv](#updating-pkenv)
+  - [License](#license)
+
+## Usage
+
+Once the plugin installed, `pkenv` will be available
+
+- Using [Antigen](https://github.com/zsh-users/antigen)
+
+Bundle `zsh-pkenv` in your `.zshrc`
+
+```shell
+antigen bundle ptavares/zsh-pkenv
+```
+
+- Using [zplug](https://github.com/b4b4r07/zplug)
+
+Load `zsh-pkenv` as a plugin in your `.zshrc`
+
+```shell
+zplug "ptavares/zsh-pkenv"
+```
+
+- Using [zgen](https://github.com/tarjoilija/zgen)
+
+Include the load command in your `.zshrc`
+
+```shell
+zget load ptavares/zsh-pkenv
+```
+
+- As an [Oh My ZSH!](https://github.com/robbyrussell/oh-my-zsh) custom plugin
+
+Clone `zsh-pkenv` into your custom plugins repo and load as a plugin in your `.zshrc`
+
+```shell
+git clone https://github.com/ptavares/zsh-pkenv.git ~/.oh-my-zsh/custom/plugins/zsh-pkenv
+```
+
+```shell
+plugins+=(zsh-pkenv)
+```
+
+Keep in mind that plugins need to be added before `oh-my-zsh.sh` is sourced.
+
+- Manually
+
+Clone this repository somewhere (`~/.zsh-pkenv` for example) and source it in your `.zshrc`
+
+```shell
+git clone https://github.com/ptavares/zsh-pkenv ~/.zsh-pkenv
+```
+
+```shell
+source ~/.zsh-pkenv/zsh-pkenv.plugin.zsh
+```
+
+## Updating pkenv
+
+The plugin comes with a zsh function to update [pkenv](https://github.com/tfutils/pkenv.git) manually
+
+```shell
+# From zsh shell
+update_zsh_pkenv
+```
+
+## License
+
+[MIT](LICENCE)
---
+++ zsh-pkenv.plugin.zsh
@@ -0,0 +1,78 @@
+#!/usr/bin/env zsh
+
+#####################
+# COMMONS
+#####################
+autoload colors
+
+#########################
+# CONSTANT
+#########################
+
+GITHUB="https://github.com"
+BOLD="bold"
+NONE="none"
+
+#########################
+# PLUGIN MAIN
+#########################
+
+[[ -z "$PKENV_HOME" ]] && export PKENV_HOME="$HOME/.pkenv"
+
+#########################
+# Functions
+#########################
+
+_zsh_pkenv_log() {
+  local font=$1
+  local color=$2
+  local msg=$3
+
+  if [ $font = $BOLD ]
+  then
+    echo $fg_bold[$color] "[zsh-pkenv-plugin] $msg" $reset_color
+  else
+    echo $fg[$color] "[zsh-pkenv-plugin] $msg" $reset_color
+  fi
+}
+
+_zsh_pkenv_install() {
+  _zsh_pkenv_log $NONE "blue" "#################################"
+  _zsh_pkenv_log $BOLD "blue" "Installing pkenv..."
+  git clone "${GITHUB}/iamhsa/pkenv.git" "${PKENV_HOME}" >> /dev/null
+  _zsh_pkenv_log $BOLD "green" "Install OK"
+  _zsh_pkenv_log $NONE "blue" "#################################"
+}
+
+update_zsh_pkenv() {
+  _zsh_pkenv_log $NONE "blue" "#################################"
+  _zsh_pkenv_log $BOLD "blue" "Updating pkenv..."
+
+  pushd "${PKENV_HOME}" > /dev/null
+  if git pull --rebase --stat origin master
+    then
+      _zsh_pkenv_log $BOLD "green" "pkenv has been updated and/or is at the last version."
+      popd > /dev/null
+    else
+      _zsh_pkenv_log $BOLD "red" "Error on updating. Please try again later."
+  fi
+  _zsh_pkenv_log $NONE "blue" "#################################"
+
+  unset _zsh_pkenv_log
+}
+
+_zsh_pkenv_load() {
+    # export PATH
+    export PATH="$PKENV_HOME/bin:$PATH"
+}
+
+
+# install pkenv if it isnt already installed
+[[ ! -f "$PKENV_HOME/bin/pkenv" ]] && _zsh_pkenv_install
+
+# load pkenv if it is installed
+if [[ -f "$PKENV_HOME/bin/pkenv" ]]; then
+    _zsh_pkenv_load
+fi
+
+unset -f _zsh_pkenv_install _zsh_pkenv_load
