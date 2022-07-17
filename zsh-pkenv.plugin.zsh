f1aed13a1dd4d2577dc9f4b9e6598c5fe4c85a0a
Patrick Tavares <tavarespatrick01@gmail.com>
2022-07-17 17:09:41 +0200

    :bug: fix duplicate path addition

--- zsh-pkenv.plugin.zsh
+++ zsh-pkenv.plugin.zsh
@@ -62,8 +62,12 @@ update_zsh_pkenv() {
 }
 
 _zsh_pkenv_load() {
-    # export PATH
-    export PATH="$PKENV_HOME/bin:$PATH"
+    # export PATH if needed
+    local -r plugin_dir="$PKENV_HOME/bin:$PATH"
+    # Add the plugin bin directory path if it doesn't exist in $PATH.
+    if [[ -z ${path[(r)$plugin_dir]} ]]; then
+        path+=($plugin_dir)
+    fi
 }
 
 
